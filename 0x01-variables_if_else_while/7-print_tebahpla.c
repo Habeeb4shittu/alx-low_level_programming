cd3aefec333779dd0f14967e12ce9be67b794e9c
Habeeb <habeeb4shittu>
2022-09-09 13:04:06 -0700

    Variables If Else While

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ztoa;
+for (ztoa = 'z'; ztoa >=  'a'; ztoa--)
+putchar(ztoa);
+putchar('\n');
+return (0);
+}
