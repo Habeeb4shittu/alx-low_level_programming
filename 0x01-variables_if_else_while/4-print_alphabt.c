f811549bc9505bedc7db4e5b3250a0ee8e871ba7
Habeeb <habeeb4shittu>
2022-09-09 12:33:14 -0700

    Variables If Else While

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,10 +7,9 @@
 int main(void)
 {
 char c;
-	
-for (c = 'a', c <= 'z', c+
-       tchar(c);
-    putchar('\n');
-		
+for (c = 'a'; c <= 'z'; c++)
+if (c != 'e' && c != 'q')
+putchar(c);
+putchar('\n')
 return (0);
 }
