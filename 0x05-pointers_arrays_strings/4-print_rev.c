46cf700d7e98a7f16aea6150bd3feda61d18e72b
Habeeb <habeeb4shittu>
2022-09-19 05:11:18 -0700

    Pointers Arrays And Strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_rev - print string in a reverse mode
+* @s: string
+* Return: Always Success
+*/
+void print_rev(char *s)
+{
+int init;
+for (init = 0; s[init] != '\0'; ++init)
+{
+}
+for (--init; init >= 0; --index)
+{
+putchar(s[index]);
+}
+putchar('\n');
+}
