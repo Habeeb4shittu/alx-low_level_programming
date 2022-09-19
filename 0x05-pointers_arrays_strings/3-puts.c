7096db27c4d30fc7871ec31144776f56f3210ce3
Habeeb <habeeb4shittu>
2022-09-19 04:34:18 -0700

    Pointers Arrys And strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _puts - prints a string, followed by a new line
+*
+* @str: string
+* Return: always success
+*/
+void _puts(char *str)
+{
+puts("%p\n", *str);
+}
