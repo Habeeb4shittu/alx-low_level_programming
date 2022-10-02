422910e4645822de506be008d42bb52651821c95
Habeeb <habeeb4shittu>
2022-10-02 12:35:13 -0700

    Argv Argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - function
+* @argc: the number of command line arguements
+* @argv: An arra that contains the program command line arguements
+* Return: 0 Always Success
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
