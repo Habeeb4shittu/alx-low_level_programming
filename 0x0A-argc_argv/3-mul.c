c5f19847d4a5ed83f49eadefe10448eea2a9eab7
Habeeb <habeeb4shittu>
2022-10-02 13:11:02 -0700

    Argv Argc

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - function
+* @argc: integer
+* @argv: array of strings
+* Return: Always success
+*/
+int main(int argc, char *argv[])
+{
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
+}
